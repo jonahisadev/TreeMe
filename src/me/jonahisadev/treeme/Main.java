b57364603a12f13b147cf6d26a1d0df281c1a66f
Jonah Alligood <jonahisadev@gmail.com>
2020-12-24 21:22:18 -0600

    Removed unnecessary stuff from Main

--- src/me/jonahisadev/treeme/Main.java
+++ src/me/jonahisadev/treeme/Main.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 
 public class Main extends JavaPlugin {
 
-    public ArrayList<Material> LOGS = new ArrayList<>();
-
     @Override
     public void onDisable() {
 
@@ -16,13 +14,6 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        LOGS.add(Material.OAK_LOG);
-        LOGS.add(Material.SPRUCE_LOG);
-        LOGS.add(Material.BIRCH_LOG);
-        LOGS.add(Material.JUNGLE_LOG);
-        LOGS.add(Material.ACACIA_LOG);
-        LOGS.add(Material.DARK_OAK_LOG);
-
         getServer().getPluginManager().registerEvents(new CutListener(this), this);
     }
 
